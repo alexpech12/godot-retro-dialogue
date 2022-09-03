1e355952fadb623b8afc3aebe23fd6fbc267aecd
Alexander Pech <alexpech12@gmail.com>
2022-09-03 10:27:32 +1000

    Add a branch to the conversation

--- DialogueUI.gd
+++ DialogueUI.gd
@@ -5,22 +5,58 @@ onready var dialogue_node = get_node("DialogueRect/Dialogue")
 onready var choice_a_node = get_node("DialogueRect/ChoiceA")
 onready var choice_b_node = get_node("DialogueRect/ChoiceB")
 
-var dialogue = [
-  "Hey there.\nDo you like apples?",
-  "I like other fruits too.",
-  "Bananas are my favourite!"
-]
+var conversation = [
+  {
+    "dialogue": "Hey there.\nDo you like apples?",
+    "choices": [
+      {
+        "dialogue": "Sure do!",
+        "destination": 1
+      },
+      {
+        "dialogue": "No way, gross!",
+        "destination": 2
+      }
+      ]
+  },
+  {
+    "dialogue": "You like apples? Me too!",
+    "destination": 3
+  },
+  {
+    "dialogue": "You don't?\nThat's a shame."
+  },
+  {
+    "dialogue": "I like other fruits too."
+  },
+  {
+    "dialogue": "Bananas are my favourite!"
+  }
+  ]
 
 var current_index = 0
 
 func _ready():
   name_node.text = "ALEX"
-  dialogue_node.text = dialogue[current_index]
+  dialogue_node.text = conversation[current_index]["dialogue"]
   choice_a_node.text = "Sure do!"
   choice_b_node.text = "No way, gross!"
-  
+
 func _process(delta):
-  if (Input.is_action_just_pressed("ui_accept")
-      and current_index < (dialogue.size() - 1)):
-    current_index += 1
-    dialogue_node.text = dialogue[current_index]
+  if current_index < (conversation.size() - 1):
+    var previous_index = current_index
+    
+    if conversation[current_index].has("choices"):
+      if Input.is_action_just_pressed("ui_up"):
+        current_index = conversation[current_index]["choices"][0]["destination"]
+        
+      if Input.is_action_just_pressed("ui_down"):
+        current_index = conversation[current_index]["choices"][1]["destination"]
+      
+    if Input.is_action_just_pressed("ui_accept"):
+      current_index = conversation[current_index].get(
+        "destination", current_index + 1
+      )
+    
+    if current_index != previous_index:
+      dialogue_node.text = conversation[current_index]["dialogue"]
